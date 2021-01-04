1a179177c834df8cb7bbe3d83ac0c7b4f222dec0
Flightkick <Flightkick@users.noreply.github.com>
2021-01-05 00:46:09 +0100

    Added: Ability to exit instances instantly by /out or keybind

--- Outkasted.lua
+++ Outkasted.lua
@@ -12,13 +12,37 @@ function Outkasted.TeleportToGuildHall()
 	end
 end
 
+function Outkasted.LeaveInstance(confirm)
+	local LEAVE_DIALOG = "INSTANCE_LEAVE_DIALOG"
+
+	if not IsUnitInDungeon("reticleover") then
+		d("ZOS reports you're currently not inside a dungeon or trial but since this API seems unstable let me try to get you out anyway.")
+	else
+		d("Exiting instance")
+	end
+
+	if confirm == false then
+		ExitInstanceImmediately()
+	else
+		if IsInGamepadPreferredMode() then
+			ZO_Dialogs_ShowGamepadDialog(LEAVE_DIALOG)
+		else
+			ZO_Dialogs_ShowDialog(LEAVE_DIALOG)
+		end
+	end
+end
+
 function Outkasted.OnAddOnLoaded(_, addonName)
 	if addonName ~= Outkasted.name then return end
+	EVENT_MANAGER:UnregisterForEvent(Outkasted.name, EVENT_ADD_ON_LOADED)
 
 	Outkasted.initMenu()
 
-	ZO_CreateStringId("SI_BINDING_NAME_TP_OUTKASTED_GUILD_HALL", "Teleport to Outkasted guild house.")
+	ZO_CreateStringId("SI_BINDING_NAME_TP_OUTKASTED_GUILD_HALL", "Teleport to Outkasted guild house")
 	SLASH_COMMANDS["/guildhall"] = function() Outkasted.TeleportToGuildHall() end
+
+	ZO_CreateStringId("SI_BINDING_NAME_LEAVE_INSTANCE", "Leave instance")
+	SLASH_COMMANDS["/out"] = function() Outkasted.LeaveInstance(false) end
 end
 
 EVENT_MANAGER:RegisterForEvent(Outkasted.name, EVENT_ADD_ON_LOADED, Outkasted.OnAddOnLoaded)
