7d19e12258cc41df5f374ab09695eaebf139a9ff
Flightkick <Flightkick@users.noreply.github.com>
2021-07-28 00:46:00 +0200

    Updated Russian translation

--- i18n/ru.lua
+++ i18n/ru.lua
@@ -1,10 +1,10 @@
 local strings = {
-    OUTK_LEAVEINSTANCE_ZOS_REPORTS_NOT_IN_TRIAL = "ZOS сообщает, что в настоящее время вы не находитесь в подземелье или в суде, но так как этот API кажется нестабильным, позвольте мне попытаться вытащить вас в любом случае.",
-    OUTK_LEAVEINSTANCE_EXITING_INSTANCE = "Выходящая инстанция",
+    OUTK_LEAVEINSTANCE_ZOS_REPORTS_NOT_IN_TRIAL = "Игра сообщает, что вы в настоящее время не в подземелье или на испытании, но т.к. этот API по-видимому нестабилен, я попробую вас вытащить.",
+    OUTK_LEAVEINSTANCE_EXITING_INSTANCE = "В процессе выхода",
 
     -- Key bindings
-    SI_BINDING_NAME_TP_OUTKASTED_GUILD_HALL = "Телепорт в дом гильдии \"Отверженные\".",
-    SI_BINDING_NAME_LEAVE_INSTANCE = "Оставить заявку",
+    SI_BINDING_NAME_TP_OUTKASTED_GUILD_HALL = "Переместиться в гильдхолл",
+    SI_BINDING_NAME_LEAVE_INSTANCE = "Покинуть местоположение",
 }
 
 for stringId, stringValue in pairs(strings) do
